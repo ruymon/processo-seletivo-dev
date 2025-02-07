74180b4348098e03811590afd92a197ee6b40ac5
Ruy Monteiro <ruystfrancis@gmail.com>
2025-02-07 16:05:35 -0300

    feat: Code during interview

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
-import { PostFilter, search } from "./search"
+import { PostFilter, search } from "./search";
 
 async function main() {
   // Você pode essa função à vontade também pra testar!
   const filter: PostFilter = {
-    title: '9',
-    kind: 'report',
-    category: 'Notícia',
-    target: 'Aluno',
-    publishedAtStart: new Date('2024-01-01'),
-    publishedAtEnd: new Date('2024-06-20'),
-    createdAtStart: new Date('2023-08-01'),
-    createdAtEnd: new Date('2023-12-20'),
-  }
+    // title: "9990",
+    // kind: "informative",
+    // category: "Notícia",
+    // target: "Aluno",
+    // publishedAtStart: new Date("2024-06-06"),
+    // publishedAtEnd: new Date("2024-06-07"),
+    // createdAtStart: new Date("2023-11-24"),
+    // createdAtEnd: new Date("2023-12-18"),
+  };
 
-  console.time('filter')
-  const posts = await search(filter)
-  console.timeEnd('filter')
+  console.time("filter");
+  const posts = await search(filter);
+  console.timeEnd("filter");
 
-  console.table(posts)
+  console.table(posts);
 }
-main()
+main();
--- search.ts
+++ search.ts
@@ -1,33 +1,104 @@
-import { readFileSync } from 'fs'
+import { readFileSync } from "fs";
+
+type PostKind = "report" | "gallery" | "video" | "banner" | "informative";
 
 export type Post = {
-  id: number
-  title: string
-  kind: 'report' | 'gallery' | 'video' | 'banner' | 'informative'
-  publishedAt: Date
-  createdAt: Date
-  category: string[]
-  targets: string[]
-}
+  id: number;
+  title: string;
+  kind: PostKind;
+  publishedAt: Date;
+  createdAt: Date;
+  category: string[];
+  targets: string[];
+};
 
 export type PostFilter = {
-  title?: string
-  kind?: 'report' | 'gallery' | 'video' | 'banner' | 'informative'
-  category?: string
-  target?: string
-  publishedAtStart?: Date
-  publishedAtEnd?: Date
-  createdAtStart?: Date
-  createdAtEnd?: Date
-}
+  title?: string;
+  kind?: PostKind;
+  category?: string;
+  target?: string;
+  publishedAtStart?: Date;
+  publishedAtEnd?: Date;
+  createdAtStart?: Date;
+  createdAtEnd?: Date;
+};
 
 export async function search(filter: PostFilter): Promise<Post[]> {
-  const posts: Post[] = JSON.parse(readFileSync('./data/posts.json', 'utf-8'))
+  const posts: Post[] = JSON.parse(readFileSync("./data/posts.json", "utf-8"));
 
   /********************************************
    *            CODIFIQUE AQUI!
    *    Você deve implementar essa função
    ********************************************/
 
-  return posts
+  let filterPosts = posts;
+
+  if (filter.title) {
+    filterPosts = filterPosts.filter((post) => {
+      const normalizedPostTitle = post.title.toLowerCase();
+      const normalizedSearchTitle = filter.title!.toLocaleLowerCase();
+
+      return normalizedPostTitle.includes(normalizedSearchTitle);
+    });
+  }
+
+  if (filter.kind) {
+    filterPosts = filterPosts.filter((post) => post.kind === filter.kind);
+  }
+
+  if (filter.category) {
+    filterPosts = filterPosts.filter((post) =>
+      post.category.includes(filter.category!)
+    );
+  }
+
+  if (filter.target) {
+    filterPosts = filterPosts.filter((post) =>
+      post.targets.includes(filter.target!)
+    );
+  }
+
+  if (filter.publishedAtStart) {
+    filterPosts = filterPosts.filter((post) => {
+      const postPublish = new Date(post.publishedAt);
+      const filterPublished = new Date(filter.publishedAtStart!);
+
+      return postPublish >= filterPublished;
+    });
+  }
+
+  if (filter.publishedAtEnd) {
+    filterPosts = filterPosts.filter((post) => {
+      const postPublish = new Date(post.publishedAt);
+      const filterPublished = new Date(filter.publishedAtEnd!);
+
+      return postPublish <= filterPublished;
+    });
+  }
+
+  if (filter.createdAtStart) {
+    filterPosts = filterPosts.filter((post) => {
+      const postCreated = new Date(post.createdAt);
+      const filterCreated = new Date(filter.createdAtStart!);
+
+      return postCreated >= filterCreated;
+    });
+  }
+
+  if (filter.createdAtEnd) {
+    filterPosts = filterPosts.filter((post) => {
+      const postCreated = new Date(post.createdAt);
+      const filterCreated = new Date(filter.createdAtEnd!);
+
+      return postCreated <= filterCreated;
+    });
+  }
+
+  return filterPosts.sort((a, b) => {
+    if (a.publishedAt >= b.publishedAt) {
+      return -1;
+    }
+
+    return 0;
+  });
 }
